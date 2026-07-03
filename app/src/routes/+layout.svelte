66180730789254426d45b675ee02fc720f796e88
Pieter Groenendijk <pieter.groenendijk@hotmail.com>
2026-07-03 22:59:35 +0200

    Favicon

--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -13,6 +13,10 @@
     <!-- SEO -->
     <title>Sifaka.dev › Database design without grunt work</title>
     <meta name="description" content="Pragmatic database design for MongoDB, PostgreSQL and other platforms. A time-efficient, easy-to-use tool that translates intent into implementation."/>
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
